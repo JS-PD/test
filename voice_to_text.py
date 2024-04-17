5fb7389d01cce6c9486d8fc9b5d74f07f4488592
JS-PD <94349961+JS-PD@users.noreply.github.com>
2024-04-17 17:13:54 +0900

    Update voice_to_text.py

--- voice_to_text.py
+++ voice_to_text.py
@@ -1,11 +1,6 @@
 from openai import OpenAI
 import streamlit as st
 
-
-client = OpenAI(
-    api_key = OPENAI_API_KEY,
-)
-
 import speech_recognition as sr
 from gtts import gTTS
 import os
@@ -55,6 +50,9 @@ st.sidebar.header("ChatGPT Demo")
 st.write(
     """This demo shows how to use `st.write` to visualize Pandas DataFrames."""
 )
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password", )
+
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -72,34 +70,39 @@ if prompt := st.chat_input():
     with st.chat_message("user"):
         st.write(prompt)
 
-completion = client.chat.completions.create(
-    #model="gpt-3.5-turbo-16k",
-    model="gpt-3.5-turbo",
-    stream=True,
-    messages=st.session_state.messages
+if openai_api_key:
+    client = OpenAI(
+        api_key = openai_api_key,
     )
 
-import time
+    completion = client.chat.completions.create(
+        #model="gpt-3.5-turbo-16k",
+        model="gpt-3.5-turbo",
+        stream=True,
+        messages=st.session_state.messages
+        )
 
-final_answer = []
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            for chunk in completion:
-                # chunk 를 저장
-                chunk_content = chunk.choices[0].delta.content
-                # chunk 가 문자열이면 final_answer 에 추가
-                if isinstance(chunk_content, str):
-                    final_answer.append(chunk_content)
-                    # 토큰 단위로 실시간 답변 출력
+    import time
+
+    final_answer = []
+    # Generate a new response if last message is not from assistant
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                for chunk in completion:
+                    # chunk 를 저장
+                    chunk_content = chunk.choices[0].delta.content
+                    # chunk 가 문자열이면 final_answer 에 추가
+                    if isinstance(chunk_content, str):
+                        final_answer.append(chunk_content)
+                        # 토큰 단위로 실시간 답변 출력
 
 
-        placeholder = st.empty()
-        full_response = ''
-        for item in final_answer:
-            full_response += item
-            placeholder.markdown(full_response)
-            time.sleep(0.02)
-    message = {"role": "assistant", "content": full_response}
-    st.session_state.messages.append(message)
+            placeholder = st.empty()
+            full_response = ''
+            for item in final_answer:
+                full_response += item
+                placeholder.markdown(full_response)
+                time.sleep(0.02)
+        message = {"role": "assistant", "content": full_response}
+        st.session_state.messages.append(message)
